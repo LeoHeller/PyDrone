ae3260c6ae4df58e252a7caa351e14ff030f7db6
Leo Heller <leojheller@gmail.com>
2019-02-08 13:02:17 +0000

    changed MPU lib

--- Droneside/gyro_output_plaintext.py
+++ Droneside/gyro_output_plaintext.py
@@ -5,14 +5,28 @@ import time
 import threading
 
 
+
+import timeit
 mpu9250 = MPU9250.MPU9250()
 
-while True:
-    accel = mpu9250.readAccel()
+def f():
     gyro = mpu9250.readGyro()
-    mag = mpu9250.readMagnet()
-    a_t = "{:=7.3f} {:=7.3f} {:=7.3f}".format(accel["x"], accel["y"], accel["z"])
-    g_t = "{:=7.3f} {:=7.3f} {:=7.3f}".format(gyro["x"], gyro["y"], gyro["z"])
-    m_t = "{:=7.3f} {:=7.3f} {:=7.3f}".format(mag["x"], mag["y"], mag["z"])
-    text = "\raccel: {}, gyro: {}, mag: {}".format(a_t, g_t, m_t)
-    print(text, end="")
+
+f()
+print(timeit.timeit(f), setup="import MPU9250")
+quit()
+
+i=0
+N = 1000
+start = time.time()
+while i < N:
+    #accel = mpu9250.readAccel()
+    gyro = mpu9250.readGyro()
+    #mag = mpu9250.readMagnet()
+    #a_t = "{:=7.3f} {:=7.3f} {:=7.3f}".format(accel["x"], accel["y"], accel["z"])
+    #g_t = "{:=7.3f} {:=7.3f} {:=7.3f}".format(gyro["x"], gyro["y"], gyro["z"])
+    #m_t = "{:=7.3f} {:=7.3f} {:=7.3f}".format(mag["x"], mag["y"], mag["z"])
+    #text = "\raccel: {}, gyro: {}, mag: {}".format(a_t, g_t, m_t)
+    #print(text, end="")
+    i += 1
+print((time.time()-start)/N)
