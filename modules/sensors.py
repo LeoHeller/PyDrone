b1231a33d2f6f626ec8095953a1598a32cb1f4d8
Leo Heller <leojheller@gmail.com>
2019-02-09 20:32:51 +0100

    switched to using Madgwick c lib

--- modules/sensors.py
+++ modules/sensors.py
@@ -6,6 +6,20 @@ import time
 import MPU9250
 
 
+from ctypes import cdll
+import ctypes
+
+lib = cdll.LoadLibrary('./libmad.so')
+lib.get_q0.restype = ctypes.c_float
+lib.get_q1.restype = ctypes.c_float
+lib.get_q2.restype = ctypes.c_float
+lib.get_q3.restype = ctypes.c_float
+py_update = lib.MadgwickAHRSupdateIMU
+py_update.argtypes = [ctypes.c_float,ctypes.c_float,ctypes.c_float,ctypes.c_float,ctypes.c_flo$
+
+
+
+
 class DoEvery(threading.Thread):
     def __init__(self, period, f):
         super().__init__()
@@ -54,23 +68,29 @@ class Sensors(threading.Thread):
 
     def read(self):
         gyro = self.mpu9250.readGyro()
+        gyro = np.multiply(gyro, 0.0174533)
         accel = self.mpu9250.readAccel()
 
-        for i in [0, 1, 2]:
-            if abs(gyro[i]) > 0.2:
-                pass
-            else:
-                gyro[i] = 0
-        self.integrate(gyro)
-        self.degrees = self.raw_integrated_gyro
-        #self.degrees = self.filter_complementary(self.raw_integrated_gyro, accel)
+        # for i in [0, 1, 2]:
+        #     if abs(gyro[i]) > 0.2:
+        #         pass
+        #     else:
+        #         gyro[i] = 0
+        py_update(gyro[0], gyro[1], gyro[2], accel[0], accel[1], accel[2])
 
-    def filter_complementary(self, integrated_gyro, accelerometer, ratio=0.95):
-        filtered = []
-        for index in [0,1,2]:
-            filtered.append(round(integrated_gyro[index] * (1-ratio) + accelerometer[index] * ratio,1))
-        return filtered
 
+    def to_euler_angles(self, q0,q1,q2,q3):
+        pitch = np.arcsin(2 * q1 * q2 + 2 * q0 * q3)
+        if np.abs(q1 * q2 + q3 * q0 - 0.5) < 1e-8:
+            roll = 0
+            yaw = 2 * np.arctan2(q1, q0)
+        elif np.abs(q1 * q2 + q3 * q0 + 0.5) < 1e-8:
+            roll = -2 * np.arctan2(q1, q0)
+            yaw = 0
+        else:
+            roll = np.arctan2(2 * q0 * q1 - 2 * q2 * q3, 1 - 2 * q1 ** 2 - 2 * q3 ** 2)
+            yaw = np.arctan2(2 * q0 * q2 - 2 * q1 * q3, 1 - 2 * q2 ** 2 - 2 * q3 ** 2)
+        return roll, pitch, yaw
 
     def stop(self):
         self._stop = True
@@ -79,6 +99,7 @@ class Sensors(threading.Thread):
         while not self._stop:
             # if self.degrees != self.last_degrees:
             # print(*self.degrees)
-            self.send(*self.degrees)
+
+            self.send(*self.to_euler_angles(lib.get_q0(), lib.get_q1(), lib.get_q2(), lib.get_q3()))
             time.sleep(0.1)
             #    self.last_degrees = self.degrees
