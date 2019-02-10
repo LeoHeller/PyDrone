147fe5555326d1f8063a9af7a86194243f00d768
Leo Heller <leojheller@gmail.com>
2019-02-10 10:59:34 +0100

    beta update

--- modules/sensors.py
+++ modules/sensors.py
@@ -48,7 +48,7 @@ class DoEvery(threading.Thread):
 
 class Sensors(threading.Thread):
     def __init__(self, send):
-        set_beta(0.25)
+        set_beta(0.5)
         self.send = send
         self.mpu9250 = MPU9250.MPU9250()
         self.DeltaTime = 0.05
