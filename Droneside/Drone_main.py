5a1abe601fc26c5614dd75cc40355b2e17fa1521
Leo Heller <leojheller@gmail.com>
2019-02-23 09:32:43 +0100

    fix

--- Droneside/Drone_main.py
+++ Droneside/Drone_main.py
@@ -45,7 +45,7 @@ def send_telemetry(x,y,z):
 
 # create the Server object and start it
 Server = Sockets.HandleSockets(
-    "192.168.10.1", 1337, "admin", mode="s", on_message=on_message)
+    "192.168.2.236", 1337, "admin", mode="s", on_message=on_message)
 Server.isDaemon = True
 Server.start()
 
